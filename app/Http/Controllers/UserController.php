1b11053793aae541df43492af0e107cdca196283
Gabriel Domingues <gabrieldominguesconsultoria@gmail.com>
2023-02-07 20:24:17 -0300

    autenticação de login e registro finalizados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -14,5 +17,22 @@ class UserController extends Controller
     public function loginUser()
     {
         return view('loginPage');
+    }
+
+    public function storeUser(Request $request)
+    {
+        $user = new User();
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->password_confirmation = Hash::make($request->password_confirmation);
+        $user->adress = $request->adress;
+        $user->adress_number = $request->adress_number;
+        $user->phone = $request->phone;
+        $user->cpf = $request->cpf;
+        $user->cep = $request->cep;
+        $user->save();
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,18 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('home');
 });
 
-//Auth::routes();
+/* autenticação */
+Auth::routes();
 
+/* home */
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/login', 'App\Http\Controllers\UserController@loginUser')->name('login');
-Route::get('/register', 'App\Http\Controllers\UserController@registerUser')->name('register');
 
+/* registro */
+Route::get('/register', 'App\Http\Controllers\UserController@registerUser')->name('register');
+Route::post('/register', 'App\Http\Controllers\UserController@storeUser')->name('store');
 
 
